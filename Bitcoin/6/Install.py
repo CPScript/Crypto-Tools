ef3d3c444397887eb75490ac9844ab8246a2478e
Chai <83523587+CPScript@users.noreply.github.com>
2023-04-27 12:28:07 -0400

    Create Install.py

---
+++ Bitcoin/6/Install.py
@@ -0,0 +1,9 @@
+import os
+from os import system
+os.system('pip install rich')
+os.system('pip install bit')
+os.system('pip install ecdsa')
+os.system('pip install requests-html')
+os.system('pip install requests-random-user-agent')
+
+print("Finished installs... now launching")
