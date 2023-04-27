8a7a27a77c4bd035993ea70c73d1d9f2f67ecba3
Chai <83523587+CPScript@users.noreply.github.com>
2023-04-27 12:37:26 -0400

    Create Install.py

---
+++ Bitcoin/3/Install.py
@@ -0,0 +1,8 @@
+import os
+from os import system
+os.system('pip install lxml')
+os.system('pip install rich')
+os.system('pip install ecdsa')
+os.system('pip install base58')
+print(" ")
+print("Finished installs... now launching")
