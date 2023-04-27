b271302d8ef1431580baf6f3e9cd2aecbac39ce7
Chai <83523587+CPScript@users.noreply.github.com>
2023-04-27 13:00:24 -0400

    Create main.py

---
+++ Bitcoin/4/main.py
@@ -0,0 +1,2 @@
+print("This is a tool that requires you to set it up manualy, please go to the following: https://github.com/BitWalls-ops/btc-recovery/tree/main")
+print("once yout there, read the ReadME.md file and set it up.")
