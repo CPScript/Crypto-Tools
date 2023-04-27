eec15a03542bc3ec50ed3c5f3e6b8699593922d8
Chai <83523587+CPScript@users.noreply.github.com>
2023-04-27 13:10:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import time
+import os
+import sys
+import subprocess
+from os  import system
+from subprocess import call
+from platform import platform
 
+
+# delete
+puk = platform()[0], platform()[1],  platform()[2], platform()[3], platform()[4], platform()[5], platform()[6]
+
+if puk == ('W', 'i', 'n', 'd', 'o', 'w', 's'):
+    delet = 'cls'
+    dr = '\\'
+else:
+    delet = 'clear'
+    dr = '/'
+
+os.system(delet)
+time.sleep(1)
+print("Loading BTC tools...")
+time.sleep(7)
+os.system(delet)
+call(["python", "Bitcoin/main.py"])
