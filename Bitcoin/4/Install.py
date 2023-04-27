e1abd0db30a9fad4439435e5802bbf91895ee6c7
Chai <83523587+CPScript@users.noreply.github.com>
2023-04-27 12:46:59 -0400

    Delete Bitcoin/4 directory

--- Bitcoin/4/Install.py
+++
@@ -1,6 +0,0 @@
-import os
-from os import system
-os.system('cd Microllect&& pip install -r requirements.txt')
-
-print(" ")
-print("Finished installs... now launching")
--- Bitcoin/4/main.py
+++
@@ -1,1 +0,0 @@
-obfuscated code has been uploaded.
